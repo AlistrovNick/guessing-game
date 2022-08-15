eccb429bb7a9a4b09f08a74c73f57b7c1d91ad5f
Mikalai Alistrau <alistrovn@gmail.com>
2022-08-15 21:28:48 +0400

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,23 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.current = Math.round((this.max + this.min) / 2);
+        return this.current;        
     }
 
     lower() {
-
+        this.setRange(this.min, this.current);
     }
 
     greater() {
-
+        this.setRange(this.current, this.max);
     }
 }
 
